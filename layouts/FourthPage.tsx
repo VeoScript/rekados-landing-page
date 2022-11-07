6cafe3d3affd4b109c9367641964d94018b857d4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-11-07 12:55:20 +0800

    style: update main layout (#2)

--- layouts/FourthPage.tsx
+++ layouts/FourthPage.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image'
 const FourthPage = () => {
   return (
     <div className="flex flex-row md:flex-col items-start md:items-center justify-start md:justify-center w-full max-w-screen-xl h-full overflow-hidden">
-      <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-full md:max-w-4xl space-y-5 md:space-y-0">
+      <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-full md:max-w-5xl space-y-5 md:space-y-0">
         <div
           className="flex flex-col space-y-3"
           data-aos="fade-right"
           data-aos-delay="500"
           data-aos-duration="1200"
         >
-          <h1 className="font-bold text-center md:text-start text-5xl text-[#F3B900]">Detailed Instructions</h1>
-          <p className="font-normal text-center md:text-start text-xl">
+          <h1 className="font-bold text-center md:text-start text-3xl md:text-5xl text-[#F3B900]">Detailed Instructions</h1>
+          <p className="font-normal text-center md:text-start text-base md:text-xl">
             The ingredients and procedures are detailed <br />
             so they can execute is easily.
           </p>
--- layouts/SixthPage.tsx
+++ layouts/SixthPage.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 const SixthPage = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-screen-xl h-full overflow-hidden">
-      <div className="flex flex-col items-center justify-between w-full max-w-4xl space-y-5">
-        <h1 className="font-bold text-5xl text-[#F3B900]">About</h1>
-        <p className="font-normal text-xl text-center">
+      <div className="flex flex-col items-center justify-between w-full space-y-5">
+        <h1 className="font-bold text-3xl md:text-5xl text-[#F3B900]">About</h1>
+        <p className="font-normal text-base md:text-xl text-center">
           Rekados is a social media app for people who want to share and <br />
           explore new recipes and dishes.
           With Rekados, you can easily find and follow the cooks and foodies that inspire you, and get recommendations for new recipes and dishes to try.
           You can also use Rekados to save your favorite recipes.
         </p>
-        <p className="font-normal text-xl text-center">
+        <p className="font-normal text-base md:text-xl text-center">
           Rekados was founded in 2022 by two food lovers who wanted to <br />
           create a space where people could connect with others who shared their passion for food.
           We believe that food is a universal language that brings people together, and we are committed to promoting exploration and discovery through our app.
